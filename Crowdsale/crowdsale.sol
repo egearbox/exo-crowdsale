4f8c18e9e4751217baf91c50bff4e599c3f0081e
Egearbox <pascal.moreno@gmail.com>
2023-02-24 00:14:41 +0100

    update change crowdsale.sol and delete >ERC20token.sol-old and new ERC20Token.sol

--- Crowdsale/crowdsale.sol
+++ Crowdsale/crowdsale.sol
@@ -1,7 +1,7 @@
 // Crowdsale.sol
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.9;
-import "../ERC20Token.sol";
+import "./ERC20Token.sol";
 contract Crowdsale {  
   uint public rate = 200; // le taux à utiliser
   ERC20Token public token;
