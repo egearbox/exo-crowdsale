f41e4ec5469766c1665c4c2cc61c61510b936372
Egearbox <pascal.moreno@gmail.com>
2023-02-24 00:17:17 +0100

    update Bank.sol

--- Bank.sol
+++ Bank.sol
@@ -1,41 +1,20 @@
-//SPDX-Licence-Identifier: MIT
-Pragma Solidity 0.8.18;
-
+// Bank.sol
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+ 
 contract Bank {
-    mapping(address => uint) balance;
-    address owner;
-    
-    constructor() {
-        owner = msg.sender;
-    }
-    
-    function deposit() public payable {
-        balance[msg.sender] += msg.value;
-    }
-    
-    function withdraw(uint amount) public {
-        require(balance[msg.sender] >= amount, "Insufficient funds");
-        balance[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
-    }
-    
-    function getBalance() public view returns(uint) {
-        return balance[msg.sender];
-    }
-    
-    function transfer(address recipient, uint amount) public {
-        require(balance[msg.sender] >= amount, "Insufficient funds");
-        require(msg.sender != recipient, "You can't transfer to yourself");
-        
-        uint previousSenderBalance = balance[msg.sender];
-        
-        _transfer(msg.sender, recipient, amount);
-        
-        assert(balance[msg.sender] == previousSenderBalance - amount);
-    }
-    
-    function _transfer(address from, address to, uint amount) private {
-        balance[from] -= amount;
-        balance[to] += amount;
-    }
+   mapping (address => uint) private _balances;
+ 
+   function deposit(uint _amount) public{
+       _balances[msg.sender] += _amount;
+   }
+   function transfer(address _recipient, uint _amount) public{
+       require(_recipient != address(0), "You cannot transfer to the address zero");
+       require(_balances[msg.sender] >= _amount, "You have not enough balance");
+       _balances[_recipient] += _amount;
+       _balances[msg.sender] -= _amount;
+   }
+   function balanceOf(address _address) public view returns (uint){
+       return _balances[_address];
+   }
 }
